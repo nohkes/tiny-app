ab74e5b36df1a627f823ecfe67cef3c475d64f28
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-10-30 20:53:21 +0000

    day 1 of tinyApp project

--- express_server.js
+++ express_server.js
@@ -1,24 +1,56 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+
+function generateRandomString() {
+	let r = Math.random().toString(36).substring(7);
+	return r;
+}
 
 var urlDatabase = { 
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "shortURL": "longURL" 
 };
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.post("/urls", (req, res) => {
+  // console.log(req.body);
+  // console.log(req.body.longURL) 
+  urlDatabase[generateRandomString()] = req.body.longURL
+  console.log(urlDatabase);// debug statement to see POST parameters
+
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+	let longURL = urlDatabase[req.params.shortURL] // using the short url against the database
+	console.log(longURL);
+	
+  res.redirect(longURL);
+});
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
-  						longURL: urlDatabase };
+  						longURL: urlDatabase    };
   res.render("urls_show", templateVars);
+
 });
 
 
